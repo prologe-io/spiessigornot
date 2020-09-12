54594bb4b6505a0e57009645f89e56776f8a68f2
Benjamin Shafii <ben@prologe.io>
2020-09-12 17:19:26 +0200

    feat: ability to upvote an item

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
-import { useFirebaseConnect } from "react-redux-firebase";
+import { View, Text, Image, Button } from "react-native";
+import { useFirebase, useFirebaseConnect } from "react-redux-firebase";
 import { useSelector } from "react-redux";
 
 export default () => {
   useFirebaseConnect("gegenstand");
+  const firebase = useFirebase()
   const gegenstand = useSelector((state) => state.firebase.ordered.gegenstand);
-  console.log(gegenstand);
+
+  const handleUpVote = (key, votes) => {
+    firebase.update(`gegenstand/${key}`, {votes:  votes + 1})
+
+  }
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {gegenstand &&
         gegenstand.map((item, index) => (
           <View key={item.key}>
             <Text>
-              {index + 1}. {item.value.name}
+              {index + 1}. {item.value.name} - {item.value.votes} votes
             </Text>
             <Image
               style={{ minWidth: 100, minHeight: 100}}
@@ -21,6 +27,7 @@ export default () => {
                 uri: item.value.photo,
               }}
             />
+            <Button onPress={() => handleUpVote(item.key, item.value.votes)} title={`✨`}/>
           </View>
         ))}
     </View>
