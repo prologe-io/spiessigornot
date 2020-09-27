a67368606fc380ee0c906b7e6ebc6ff002f2dadd
Benjamin Shafii <ben@prologe.io>
2020-09-27 17:27:46 +0200

    style: add elevation to cards

--- Card.js
+++ Card.js
@@ -1,13 +1,34 @@
+import React from "react";
 import styled from "styled-components/native";
 
-export const Card = styled.View`
+export const StyledCard = styled.View`
   max-width: 350px;
-  background-color: rgb(242, 242, 242);
   padding: 16px;
+  min-height: 300px;
+  margin: 16px;
+  background-color: white;
 `;
 
+export const Card = ({ children, ...rest }) => (
+  <StyledCard
+    {...rest}
+    style={{
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.23,
+      shadowRadius: 2.62,
+
+      elevation: 4,
+    }}
+  >
+    {children}
+  </StyledCard>
+);
 
 export const CardTitle = styled.Text`
-    margin-bottom: 16px;
-    max-width: 180px;
-`
+  margin-bottom: 16px;
+  max-width: 180px;
+`;
