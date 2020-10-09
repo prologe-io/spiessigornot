e72382e37386bc00a140118ef057b2ff04352df5
Benjamin Shafii <ben@prologe.io>
2020-10-09 13:51:29 +0200

    chore: replace app.json by app.config.js

--- app.config.js
+++ app.config.js
@@ -6,6 +6,30 @@ const allowedEmails =
     : "";
 
 export default {
+  name: "spiessigornot",
+  slug: "spiessigornot",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  android: {
+    package: "com.prologe.spiessigornot",
+    versionCode: 3,
+  },
   extra: {
     apiKey: process.env.EXPO_API_KEY,
     authDomain: process.env.EXPO_AUTH_DOMAIN,
